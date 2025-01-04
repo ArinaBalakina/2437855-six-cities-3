7e486011a35f7435117ba09f3a5daddfdb813b78
ArinaBalakina <Balakinaarina@yandex.ru>
2025-01-05 01:23:02 +0500

    финал

--- src/cli/commands/version.command.ts
+++ src/cli/commands/version.command.ts
@@ -1,35 +1,32 @@
-import { inject, injectable } from 'inversify';
-import {readFileSync} from 'node:fs';
-import {resolve} from 'node:path';
 import {Command} from './command.interface.js';
-import { Component } from '../../types/index.js';
-import { Logger } from '../../logger/index.js';
+import {version} from '../../../package.json';
+import chalk from 'chalk';
 
-
-@injectable()
 export class VersionCommand implements Command {
-    constructor(
-        @inject(Component.Logger) private readonly logger: Logger,
-        private readonly filePath: string = './package.json'
-    ) {}
+  constructor(private readonly file: string = 'package.json') {}
 
-    public getName(): string {
-        return '--version';
+  private readVersion(): string {
+    if (!version) {
+      throw new Error(`Failed to import version from ${this.file}.`);
+    }
+
+    return version;
+  }
+
+  public getName(): string {
+    return '--version';
+  }
+
+  public async execute(..._parameters: string[]): Promise<void> {
+    try {
+      const v = this.readVersion();
+      console.info(chalk.green(v));
+    } catch (error: unknown) {
+      console.error(`Failed to read version from ${this.file}`);
+
+      if (error instanceof Error) {
+        console.error(chalk.red(error.message));
       }
-
-    private readVersion(): string {
-        const jsonContent = readFileSync(resolve('./package.json'), 'utf-8');
-        const importedContent = JSON.parse(jsonContent);
-
-        return importedContent.version;
     }
-
-    public async execute(..._parameters: string[]): Promise<void> {
-        try {
-          const version = this.readVersion();
-          this.logger.info(version);
-        } catch (error: unknown) {
-          this.logger.error(`Failed to read version from ${this.filePath}`, error as Error);
-        }
-    }
+  }
 }
